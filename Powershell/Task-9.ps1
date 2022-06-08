889c23ba36cc6e1672b54e5a8f333c73b67e41f8
Abrar Ahmed <102857826+Abrar-Ahmed-3-0@users.noreply.github.com>
2022-06-08 14:53:33 +0200

    Create Task-9.ps1

---
+++ Powershell/Task-9.ps1
@@ -0,0 +1,16 @@
+<#
+Write a script that writes out the factorial of a given number
+#>
+
+function Get-Factorial ($n) {
+    if ($n -eq 0) {
+        return 1
+    }
+    $fact = 1
+    1..$n | ForEach { $fact *= $_ }
+    return $fact
+}
+
+foreach ($i in $args[0]) {
+    echo ("{0}! = {1}" -f $i,(Get-Factorial $i))
+}
